dd53538abdf4232b86b5c69664a27f5a9b8ea3ca
Haoze Zhang <zhz1031108486@gmail.com>
2022-09-26 18:48:49 -0600

    Add files via upload

---
+++ Assembler/assembler/assembler/src/assembler/registers.java
@@ -0,0 +1,16 @@
+
+package assembler;
+
+/**
+ * This is the assembler class
+ * @author prime: Haoze Zhang		second: Yuntong Lu
+ *
+ */
+
+public class registers {
+	//The value in register file
+	int la;
+	public registers(){
+		la = 0;
+	}
+}
